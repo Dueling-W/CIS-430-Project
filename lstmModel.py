19550bff2762c5aa629773e4460cd62a11a1b419
William Girard <wgirard@umassd.edu>
2024-12-06 19:40:33 -0500

    New parameters with lstm model.

--- lstmModel.py
+++ lstmModel.py
@@ -9,14 +9,15 @@ from keras.layers import Embedding
 from keras.layers import LSTM
 from keras.layers import Dense
 from keras.layers import InputLayer
+from keras.layers import Dropout
+from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 from sklearn.preprocessing import OrdinalEncoder
 
 
 def createModel(unique_words, max_len):
 
-    opt = Adam(learning_rate=0.003)
-    output_dim = 50
+    output_dim = 32
     model = Sequential()
     print(f'Creating embed layer using {unique_words} words, {max_len} length, output dim of {output_dim}')
 
@@ -24,9 +25,11 @@ def createModel(unique_words, max_len):
     model.add(input)
     embed = Embedding(input_dim = unique_words, output_dim=output_dim, trainable=True)
     model.add(embed)
-    model.add(LSTM(100))
-    model.add(Dense(1, activation='tanh'))
+    model.add(LSTM(40))
+    model.add(Dropout(0.1))
+    model.add(Dense(5, activation='sigmoid'))
 
+    opt = Adam(learning_rate=0.003)
     model.compile(optimizer=opt, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     return model
@@ -42,7 +45,10 @@ if __name__ == "__main__":
     model = createModel(count, length)
     print(model.summary())
 
-    #model.fit(data_train, labels_train, epochs=20, validation_split=0.2, verbose=2)
+    early_stop = EarlyStopping(monitor="val_accuracy", patience=10)
+
+
+    model.fit(data_train, labels_train, epochs=100, validation_split=0.2, verbose=2, callbacks=[early_stop])
 
 
 
