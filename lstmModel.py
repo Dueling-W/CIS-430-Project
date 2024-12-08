8ecff937e21d87ef3ec90923f3ea24fb8b8b8929
Dueling-W <wgirardpm@gmail.com>
2024-12-08 11:57:42 -0500

    Added in the import statements for GRU and SimpleRNN.

--- lstmModel.py
+++ lstmModel.py
@@ -8,6 +8,8 @@ from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Embedding
 from keras.layers import LSTM
+from keras.layers import GRU
+from keras.layers import SimpleRNN
 from keras.layers import Dense
 from keras.layers import InputLayer
 from keras.layers import Dropout
